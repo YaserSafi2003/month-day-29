4721935e939320b0fb90a20f4d92ff19a145cb4a
YaserSafi2003 <108479085+YaserSafi2003@users.noreply.github.com>
2022-10-25 17:50:43 +0200

    Add files via upload

---
+++ play.js
@@ -0,0 +1,15 @@
+const month = prompt("enter the month");
+
+day = 29
+month = december
+horoscope = SAGITTARIUS
+
+if (month == "december"){
+         
+    if (day < 29)
+    astro_sign = "SAGITTARIUS";
+    else
+    astro_sign ="capricorn";
+}
+
+console.log("print the console");
